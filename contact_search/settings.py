89f2ba77c6fb1a61d6fa70052088fe07ead2eac6
abishek-v-git <abishekvaidhiyanathan@inesssolutions.com>
2026-03-10 17:49:34 +0530

    host update ,again

--- contact_search/settings.py
+++ contact_search/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-ey*j(mts()ceje+xfklq9!pjyb)&3big1+*7!02g727z%o3qx&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
